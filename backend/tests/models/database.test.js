6989ebdea7e5bc77315c9dd03e31d2889a283cca
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-09-04 16:56:47 -0700

    updated database tests

--- backend/tests/models/database.test.js
+++ backend/tests/models/database.test.js
@@ -1,17 +1,17 @@
 const pool = require("../../src/config/database");
 
-test("Checking connection to database", async () => {
-  const res = await pool.query("SELECT NOW()");
-  expect(res).toBeDefined();
-  expect(res.rows.length).toBe(1);
-});
-
-test("The connection to database fails", async () => {
-    await expect(pool.query("SELECT * FROM nonexistantTable"))
-        .rejects
-        .toThrow();
-});
-
-afterAll(async () => {
+describe("database connection", () => {
+  afterAll(async () => {
     await pool.end();
+  });
+  test("Checking connection to database", async () => {
+    const res = await pool.query("SELECT NOW()");
+    expect(res).toBeDefined();
+    expect(res.rows.length).toBe(1);
+  });
+  test("Error catcher for database failing", async () => {
+    await expect(
+      pool.query("SELECT * FROM nonexistantTable")
+    ).rejects.toThrow();
+  });
 });
