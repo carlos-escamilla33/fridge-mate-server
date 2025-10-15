cd93b403d2faaaf164e66a468dc3f7e4aceb0a44
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-10-15 11:04:59 -0700

    completed sendEmail util function

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,8 +1,35 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASS = process.env.EMAIL_PASS;
+const EMAIL_HOST = process.env.EMAIL_HOST;
 
-const sendEmail = async () => {
+const sendEmail = async (email, resetToken) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: `${EMAIL_HOST}`,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: `${EMAIL_USER}`,
+        pass: `${EMAIL_PASS}`,
+      },
+    });
 
-}
+    const info = await transporter.sendMail({
+        from: `${EMAIL_HOST}`,
+        to: `${email}`,
+        subject: `reset your password here: http://localhost:3000/api/auth/change-password?token=${resetToken}`,
+        text: "Hello world?",
+    });
+
+    console.log("Message sent:", info.messageId);
+
+  } catch (err) {
+    throw err;
+  }
+};
 
 module.exports = {
-    sendEmail,
-}
+  sendEmail,
+};
