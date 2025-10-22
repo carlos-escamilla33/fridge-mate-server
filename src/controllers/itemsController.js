e44db786f4d9bc2d8abf56075c8e651f6dc880f2
Carlos Escamilla <carlosaescamilla3@gmail.com>
2025-10-22 11:59:58 -0700

    fixed item creation date issue

--- src/controllers/itemsController.js
+++ src/controllers/itemsController.js
@@ -4,7 +4,7 @@ const itemSchema = Joi.object({
         profileId: Joi.number().integer().positive().required(),
         recipeId: Joi.number().integer().allow(null).optional(),
         foodName: Joi.string().min(2).max(100).required(),
-        expirationDate: Joi.date().required(),
+        expirationDate: Joi.string().pattern(/^\d{4}-\d{2}-\d{2}$/).required(),
         ripenessLevel: Joi.string().min(3).max(30).required()
 });
 
